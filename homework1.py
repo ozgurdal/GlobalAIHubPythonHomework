bef0d3cacd939ba8d9896a5c59594e175fd0c0e2
ozgurdal <ozgurdal92@gmail.com>
2020-12-25 18:52:43 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 17:13:07 2020
+
+@author: ozgur
+"""
+
+a = int(input("Please enter an int type value"))
+print(f"the value you entered: {a}")
+print("The type of the value you entered:{}".format(type(a)))
+b = float(input("Please enter a float type value"))
+print(f"the value you entered: {b}")
+print("The type of the value you entered:{}".format(type(b)))
+c = str(input("Please enter a str type value"))
+print(f"the value you entered: {c}")
+print("The type of the value you entered:{}".format(type(c)))
+d = input("Please enter a value")
+print(f"the value you entered: {d}")
+print("The type of the value you entered:{}".format(type(d)))
+e = input("Please enter a value")
+print(f"the value you entered: {e}")
+print("The type of the value you entered:{}".format(type(e)))
